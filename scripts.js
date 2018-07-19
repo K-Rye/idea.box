70d706ff97c23745c644b17b98747955555d4fe1
Kevin Ryan <kevinpatryan@gmail.com>
2018-07-18 22:23:19 -0600

    Final refactor commit

--- scripts.js
+++ scripts.js
@@ -1,81 +1,79 @@
-//Ideas: keyup event listener for search bar
+//============================================
+// Universal Variables
+//============================================
 var titleInput = $(".title-input");
 var bodyInput = $(".body-input");
 var saveBtn = $(".save-btn");
 var searchInput = $(".search-input");
 var ideasSection = $(".ideas-section");
 var ideaList = $(".idea-list");
-var ideaTitle = $(".idea-title");
+var ideaTitle = $(".idea-title")
 var arrayOfObject = getStoredIdeas() || [];
-var wholeWindow = $(window);
-var num = 340;
+var wholeWindow = $(window)
+var num = 340; //number of pixels the search bar slides down before becoming fixed.
 
-// ==========================
+//============================================
 // Run on page load
-// ==========================
+//============================================
+
 displayIdeas();
 
-// =========================
+//============================================
 // Event Listeners
-// =========================
+//============================================
+
 saveBtn.on("click", createIdea);
 searchInput.on("keyup", searchFunction);
 wholeWindow.bind("scroll", stickySearch);
-ideasSection.on("click", ".delete-btn", deleteArticle);
-ideasSection.on("click", ".up-btn", upQuality);
-ideasSection.on("click", ".down-btn", downQuality);
 ideasSection.on("keyup", ".idea-title", editTitleText);
 ideasSection.on("keyup", ".idea-body-txt", editBodyText);
+ideasSection.on("click", ".up-btn", upQuality);
+ideasSection.on("click", ".down-btn", downQuality);
+ideasSection.on("click", ".delete-btn", deleteArticle);
 ideasSection.on("keydown", ".idea-title", enterKeySubmits);
 ideasSection.on("keydown", ".idea-body-txt", enterKeySubmits);
 
-// =========================
+//============================================
 // Functions
-// =========================
+//============================================
 
 function createIdea(e) {
   e.preventDefault();
   var newTitle = titleInput.val();
   var newBody = bodyInput.val();
-  var newIdea = new IdeaObject(newTitle, newBody);
-
+  var newIdea = new IdeaObject(newTitle, newBody)
   arrayOfObject.push(newIdea);
   var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringedIdea);
+  localStorage.setItem('listIdea', stringedIdea);
   displayIdeas();
 };
 
 function displayIdeas() {
-  var retrievedIdea = localStorage.getItem("listIdea");
+  var retrievedIdea = localStorage.getItem('listIdea');
   var parsedRetrievedIdea = JSON.parse(retrievedIdea);
-  if (parsedRetrievedIdea != null) {
-    ideasSection.text("");
+  if (parsedRetrievedIdea !== null) {
+    ideasSection.text('')
     parsedRetrievedIdea.forEach(idea => {
-      ideasSection.prepend
-        (`<article class="idea-list" data-unid=${idea.uniqueID}">
-          <h3 class="idea-title" contenteditable>${idea.title}</h3><img class="btn delete-btn" src="delete.svg" onmouseover="this.src="delete-hover.svg"; "onmouseout="this.src="delete.svg";">
-          <p class="idea-body-txt" contenteditable>${idea.body}</p>
-          <div class="vote-form">
-            <img class="btn up-btn" src"upvote.svg" onmouseover="this.src="upvote-hover.svg";" onmouseout="this.src="upvote.svg";"><img class="btn down-btn" src="downvote.svg" onmouseover="this.src"downvote-hover.svg";" onmouseout="this.src="downvote.svg";"><p class="quality">quality: ${idea.quality}</p>
-          </div>
-          </article>`)   
+      ideasSection.prepend(
+      `<article class="idea-list" data-unid="${idea.uniqueID}">
+        <h3 class="idea-title" contenteditable>${idea.title}</h3><img class="btn delete-btn" src="delete.svg" onmouseover="this.src='delete-hover.svg';" onmouseout="this.src='delete.svg';">
+        <p class="idea-body-txt" contenteditable>${idea.body}</p>
+        <div class="vote-form">
+          <img class="btn up-btn" src="upvote.svg" onmouseover="this.src='upvote-hover.svg';" onmouseout="this.src='upvote.svg';"><img class="btn down-btn" src="downvote.svg" onmouseover="this.src='downvote-hover.svg';" onmouseout="this.src='downvote.svg';"><p class="quality">quality: ${idea.quality}</p>
+        </div>
+      </article>`
+      )
     })
+    titleInput.val('');
+    bodyInput.val('');
   }
 };
 
 function IdeaObject(title, body) {
   this.title = title;
   this.body = body;
-  this.uniqueID = Date.now();
-  this.quality = "swill";
-};
-
-function stickySearch() {
-  if (wholeWindow.scrollTop() > num) {
-    $(".search-parent").addClass("fixed");
-  } else {
-     $(".search-parent").removeClass("fixed");
-  }
+  this.uniqueID = Date.now(); 
+  this.quality = 'swill';
 };
 
 function searchFunction() {
@@ -89,95 +87,91 @@ function searchFunction() {
   })
 };
 
-function enterKeySubmits(e) {
-  if (er.keyCode == 13 && !e.shiftKey) {
-    e.preventDefault();
-    e.target.blur();
-  }
+function stickySearch() {
+    if (wholeWindow.scrollTop() > num) {
+        $('.search-parent').addClass('fixed');
+    } else {
+        $('.search-parent').removeClass('fixed');
+    }
 };
 
 function editTitleText(e) {
-  var thisArticleId = $(event.target).parent().data('unid');
+  var thisArticleId = $(event.target).parent().data("unid");
   var thisArticleTitleText = $(event.target).text();
-  var changeThisArticle = arrayOfIdeas.filter(function (anything) {
+  var changeThisArticle = arrayOfObject.filter(function (anything) {
     if (anything.uniqueID == thisArticleId) {
       anything.title = thisArticleTitleText;
     }
   })
   var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringedIdea);
+  localStorage.setItem('listIdea', stringedIdea);
 };
 
 function editBodyText() {
-  var thisArticleId = $(event.target).parent.data("unid");
+  var thisArticleId = $(event.target).parent().data("unid");
   var thisBodyText = $(event.target).text();
   var changeThisArticle = arrayOfObject.filter(function (anything) {
     if (anything.uniqueID == thisArticleId) {
-      anything.body =thisBodyText;
+      anything.body = thisBodyText;
     }
   })
   var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringedIdea);
+  localStorage.setItem('listIdea', stringedIdea);
+};
+
+function enterKeySubmits(e) {
+  if (e.keyCode == 13 && !e.shiftKey) {
+      e.preventDefault();
+      e.target.blur();
+  }
 };
 
 function getStoredIdeas() {
-  var retirevedIdea = localStorage.getItem("listIdea");
-  var parsedRetrievedIdea = JSON.parse(retirevedIdea);
+  var retrievedIdea = localStorage.getItem('listIdea');
+  var parsedRetrievedIdea = JSON.parse(retrievedIdea);
   return parsedRetrievedIdea;
-};
-
-function deleteArticle() {
-  var thisArticleID = $(event.target).parent().data("unid")
-  var deleteThisArticle = arrayOfObject.filter(function (anything) {
-    return anything.uniqueID !== thisArticleID;
-  })
-  arrayOfObject = deleteThisArticle;
-
-  var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringedIdea);
-  $(event.target).parent().remove();
 };
 
 function upQuality() {
   var thisArticleId = $(event.target).parent().parent().data("unid")
-  var upThisArticle = arraOfObject.filter(function (anything) {
+  var upThisArticle = arrayOfObject.filter(function (anything) {
     if (anything.uniqueID == thisArticleId) {
-      if (anything.quality == "swill") {
-        anything.quality = "plausible";
-      } else if (anything.quality == "plausible") {
-        anything.quality = "genius";
+      if (anything.quality == 'swill') {
+        anything.quality = 'plausible';
+      } else if (anything.quality == 'plausible') {
+        anything.quality = 'genius';
       }
     }
   })
   var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringIdea);
+  localStorage.setItem('listIdea', stringedIdea);
   displayIdeas();
 };
 
 function downQuality() {
   var thisArticleId = $(event.target).parent().parent().data("unid")
   var downThisArticle = arrayOfObject.filter(function (anything) {
-    if (anything.uniquId == thisArticleId) {
-      if (anything.quality == "genius") {
-        anything.quality = "plausible";
-      } else if (anything.quality == "plausible") {
-        anything.quality = "swill"
+    if (anything.uniqueID == thisArticleId) {
+      if (anything.quality == 'genius') {
+        anything.quality = 'plausible';
+      } else if (anything.quality == 'plausible') {
+        anything.quality = 'swill';
       }
     }
   })
   var stringedIdea = JSON.stringify(arrayOfObject);
-  localStorage.setItem("listIdea", stringedIdea);
+  localStorage.setItem('listIdea', stringedIdea);
   displayIdeas();
 };
 
-
-
-
-
-
-
-
-
-
-
-
+function deleteArticle() {
+  var thisArticleId = $(event.target).parent().data("unid")
+  var deleteThisArticle = arrayOfObject.filter(function (anything) {
+    return anything.uniqueID !== thisArticleId;
+  })
+  arrayOfObject = deleteThisArticle;
+  
+  var stringedIdea = JSON.stringify(arrayOfObject);
+  localStorage.setItem('listIdea', stringedIdea);
+  $(event.target).parent().remove()
+};
